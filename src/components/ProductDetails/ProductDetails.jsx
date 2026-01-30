73a3344e2e1ef7575e65b1452d5c3de7b5ee8c4a
Nayana Harish <harish0878@saskpolytech.ca>
2026-01-30 11:04:06 +0530

    create a useState hook to store product details and bind it with UI

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import "./ProductDetails.css"
+import { useState } from 'react';
 
 function ProductDetails() {
+// create a usestate to store the product details object
+const [product,setProduct] = useState({
+  "id":0,
+  "image":"",
+  "title":"",
+  "rating":{
+    "rate":0,
+    "count":0
+  },
+  "category":"",
+  "description":"",
+  "price":0
+});
+
+
   return (
     <div className='product-details'>
       <div className='image'>
-        <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png" alt="" width="100%" height="470px" />
+        {product.id}
+        <img src={product.image} alt="" width="100%" height="470px" />
       </div>
       {/* for details */}
       <div className='details'>
           <div className='product-title'>
-              <h1>Lorem ipsum dolor sit amet.</h1>
+              <h1>{product.title}</h1>
           </div>         
           <div className='product-rating'>
-            <p>4.8</p>
+            <p>{product.rating.rate}</p>
             <p>****</p>
-            <p>(147)</p>
+            <p>({product.rating.count})</p>
           </div>
           <div className='product-price'>
-            <cite>$</cite><h2>360</h2>
+            <cite>$</cite><h2>{product.price}</h2>
           </div>
            <div className='product-description'>
-            <p>product description Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, rerum.
-              Lorem ipsum dolor sit amet.
+            <p>{product.description}
             </p>
           </div>
           <div className='product-button'>
